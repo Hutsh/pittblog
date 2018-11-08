2dd48d55020e751624a4ff33454563bdee6ab49a
hutsh <hxbbun@gmail.com>
2018-11-08 00:04:33 -0500

    add comment

--- index.js
+++ index.js
@@ -56,6 +56,12 @@ app.use(function (req, res, next) {
 // 路由
 routes(app)
 
+app.use(function (err, req, res, next) {
+  console.error(err)
+  req.flash('error', err.message)
+  res.redirect('/posts')
+})
+
 // 监听端口，启动程序
 app.listen(config.port, function () {
   console.log(`${pkg.name} listening on port ${config.port}`)
--- models/posts.js
+++ models/posts.js
@@ -1,5 +1,7 @@
 const marked = require('marked')
 const Post = require('../lib/mongo').Post
+const CommentModel = require('./comments')
+
 
 // 将 post 的 content 从 markdown 转换成 html
 Post.plugin('contentToHtml', {
@@ -29,6 +31,7 @@ module.exports = {
       .findOne({ _id: postId })
       .populate({ path: 'author', model: 'User' })
       .addCreatedAt()
+      .addCommentsCount()
       .contentToHtml()
       .exec()
   },
@@ -53,5 +56,52 @@ module.exports = {
     return Post
       .update({ _id: postId }, { $inc: { pv: 1 } })
       .exec()
-  }
+  },
+
+  // 通过文章 id 获取一篇原生文章（编辑文章）
+  getRawPostById: function getRawPostById (postId) {
+    return Post
+      .findOne({ _id: postId })
+      .populate({ path: 'author', model: 'User' })
+      .exec()
+  },
+
+  // 通过文章 id 更新一篇文章
+  updatePostById: function updatePostById (postId, data) {
+    return Post.update({ _id: postId }, { $set: data }).exec()
+  },
+
+  // 通过用户 id 和文章 id 删除一篇文章
+  delPostById: function delPostById (postId, author) {
+  return Post.deleteOne({ author: author, _id: postId })
+    .exec()
+    .then(function (res) {
+      // 文章删除后，再删除该文章下的所有留言
+      if (res.result.ok && res.result.n > 0) {
+        return CommentModel.delCommentsByPostId(postId)
+      }
+    })
 }
+}
+
+///////////comments
+// 给 post 添加留言数 commentsCount
+Post.plugin('addCommentsCount', {
+  afterFind: function (posts) {
+    return Promise.all(posts.map(function (post) {
+      return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
+        post.commentsCount = commentsCount
+        return post
+      })
+    }))
+  },
+  afterFindOne: function (post) {
+    if (post) {
+      return CommentModel.getCommentsCount(post._id).then(function (count) {
+        post.commentsCount = count
+        return post
+      })
+    }
+    return post
+  }
+})
